f4f7e9ea83b582505bd1c88246ea5d4474b6e261
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-05-17 12:47:26 +0200

    add mappa bandiere

--- script.js
+++ script.js
@@ -7,12 +7,7 @@ new Vue({
         tvSeriesList: []
     },
 
-    langFlagCountry = {
-        "en":["gb","us","ca","ie","au"],
-        "es":["es","ar","ct","mx"],
-        "fr":["fr","be","lu","mc"],
-        "pt":["pt","br","ao","cv"]
-    }, 
+
 
 
     methods: {
