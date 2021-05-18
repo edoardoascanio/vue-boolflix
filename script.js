1ff03b14312bfa14d0eb92c3034d14771fb0cb1a
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-05-18 11:47:16 +0200

    add loading

--- script.js
+++ script.js
@@ -4,23 +4,35 @@ new Vue({
         tmdbApiKey: "b75629beac361467f451fddc3d496e90",
         textToSearch: "",
         moviesList: [],
-        tvSeriesList: []
+        tvSeriesList: [],
+        loading: false 
+
     },
 
+    computed: {
+        fullList() {
+            return this.moviesList.concat(this.tvSeriesList)
+        }
+    }, 
 
     methods: {
 
         makeAxiosSearch(searchType) {
+
+            this.loading = true;
+
             const axiosOption = {
                 params: {
                     api_key: this.tmdbApiKey,
                     query: this.textToSearch,
-                    language: "it-IT"
+                    language: "it-IT",
                 }
             };
 
             axios.get("https://api.themoviedb.org/3/search/" + searchType, axiosOption)
                 .then((resp) => {
+
+                    this.loading = false;
 
                     if (searchType === "movie") {
                         this.moviesList = resp.data.results;
@@ -33,43 +45,64 @@ new Vue({
 
                             return tvShow
                         });
-                    }    
+                    }
                 })
 
-            const vote = (Math.round(vote_average) / 2)  
+            axios.get("https://api.themoviedb.org/3/search/" + searchType, axiosOption)
+                .then(resp => {
+
+                    if (searchType === "movie") {
+                        this.moviesList = resp.data.results;
+
+                    } else if (searchType === "tv") {
+
+                        this.tvSeriesList = resp.data.results.map((tvShow) => {
+                            tvShow.original_title = tvShow.original_name
+                            tvShow.title = tvShow.name;
+                            tvShow.tvSeries = true
+
+                            return tvShow
+                        });
+                    }
+                })
         },
 
-        getFlag(movie){
-          const langFlagsMap = {
-                en:["gb","us","ca","ie","au"],
-                es:["es","ar","ct","mx"],
-                fr:["fr","be","lu","mc"],
-                pt:["pt","br","ao","cv"]
+        getStar(movie) {
+            const vote = (Math.round(vote_average) / 2)
+            return vote
+        },
+
+        getFlag(movie) {
+            const langFlagsMap = {
+                en: ["gb", "us", "ca", "ie", "au"],
+                es: ["es", "ar", "ct", "mx"],
+                fr: ["fr", "be", "lu", "mc"],
+                pt: ["pt", "br", "ao", "cv"]
             };
 
-            if(langFlagsMap[movie.original_language]){
+            if (langFlagsMap[movie.original_language]) {
                 return langFlagsMap[movie.original_language];
-            }else{
+            } else {
                 return movie.original_language;
             }
 
         },
 
-        getImgSrc(movie){
+        getImgSearch(movie) {
 
-           if(movie.poster_path){
-               return `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-           }else{
-               return "../img/poster_placeholder.svg"
-           }
+            if (movie.poster_path) {
+                return `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+            } else {
+                return "../img/poster_placeholder.svg"
+            }
         },
 
         search() {
             this.textToSearch
-      
+
             this.makeAxiosSearch("movie");
             this.makeAxiosSearch("tv");
-        }, 
+        },
     }
 })
 
